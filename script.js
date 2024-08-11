aa927d8edd07ec5fe6afaa48b5595dd9516bfa9d
Vivek Kilari <vkilariofficial@gmail.com>
2024-08-11 01:46:57 -0400

    Added hero background animation and again reorganized section with flex

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+const hero = document.getElementById("hero");
+
+
 function revealGame(id) {
     document.getElementById(String(id)).style.display = 'none';
+}
+
+window.onmousemove = e => {
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+
+  const xDecimal = mouseX / window.innerWidth;
+  const yDecimal = mouseY / window.innerHeight;
+
+  const maxX = hero.offsetWidth - window.innerWidth;
+  const maxY = hero.offsetHeight - window.innerHeight;
+
+  console.log(maxY);
+
+  const panX = maxX * xDecimal * -.1;
+  const panY = maxY * yDecimal * -.1;
+
+  hero.translate = `translate(${panX}px, ${panY}px)`;
+
+  hero.animate({
+    transform: `translate(${panX}px, ${panY}px)`
+  }, {
+    duration: 4000,
+    fill: "forwards",
+    easing: "ease"
+  })
 }
