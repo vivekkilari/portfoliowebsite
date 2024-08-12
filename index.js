57473afa9527aa9778b51935de410383e1f89af0
Vivek Kilari <vkilariofficial@gmail.com>
2024-08-11 20:53:23 -0400

    Renamed colors, started work on game carousel and reorganized layout. Also rewrote reveal card mechanic to be data based

--- index.js
+++ index.js
@@ -1,48 +1,92 @@
 const hero = document.getElementById("hero");
+const titleShadow = document.getElementById("titleShadow");
+const gameCarousel = document.querySelector(".gameCarousel");
+const glasses = document.querySelectorAll(".glass");
 
-var panX;
-var panY;
-var rate;
-
-function revealGame(id) {
-    document.getElementById(String(id)).style.display = 'none';
-}
+var mouseX = 0;
+var mouseY = 0;
+var panX = 0;
+var panY = 0;
+var rate = 0;
 
 window.onmousemove = e => {
-  const mouseX = e.clientX;
-  const mouseY = e.clientY;
+  getMousePosition(e);
+  animateHero(e);
+  carouselMove(e);
+}
 
+window.onmousedown = e => {
+  gameCarousel.dataset.mouseDownAt = e.clientX;
+}
+
+window.onmouseup = e => {
+  gameCarousel.dataset.mouseDownAt = "0";
+}
+
+gameCarousel.addEventListener("click", e => {
+
+})
+
+window.addEventListener("scroll", e => {
+  heroParallax();
+})
+
+function getMousePosition(e) {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+}
+
+function animateHero(e) {
   const xDecimal = mouseX / window.innerWidth;
   const yDecimal = mouseY / window.innerHeight;
-
+  
   const maxX = hero.offsetWidth - window.innerWidth;
   const maxY = hero.offsetHeight - window.innerHeight;
+  
+  panX = maxX * xDecimal * -.05;
+  panY = maxY * yDecimal * -.05;
+  
+  hero.animate({
+    transform: `translate(${panX}px, ${panY + rate}px)`
+  }, {
+    duration: 1000,
+    fill: "forwards",
+    easing: "ease"
+  })
+}
 
-  panX = maxX * xDecimal * -.1;
-  panY = maxY * yDecimal * -.1;
+function carouselMove(e) {
+  if(gameCarousel.dataset.mouseDownAt === "0") return;
+  const mouseDelta = parseFloat(gameCarousel.dataset.mouseDownAt) - e.clientX;
+  const maxDelta = window.innerWidth / 2;
+
+  const percentage = (mouseDelta / maxDelta) * -100;
+
+  gameCarousel.style.transform = `translate(${percentage}%)`;
+}
+
+function revealGame(id) {
+  hideAllGames()
+  document.getElementById(String(id)).dataset.activeStatus = 'active';
+}
+
+function hideAllGames() {
+
+  glasses.forEach((g) => {
+    g.dataset.activeStatus = "inactive";
+  });
+}
+
+function heroParallax() {
+  var scrolled = window.scrollY;
+  rate = scrolled * .33;
+
 
   hero.animate({
     transform: `translate(${panX}px, ${panY + rate}px)`
   }, {
-    duration: 500,
     fill: "forwards",
-    easing: "ease"
-  })
+    easing: "ease-in-out"
+  });
 }
 
-window.addEventListener('scroll', e => {
-  const target = document.getElementById("hero");
-
-  var scrolled = window.scrollY;
-  rate = scrolled * 0.5;
-  
-
-  hero.animate({
-    transform: `translate(${panX}px ,${panY + rate}px)`
-  }, {
-    duration: 1,
-    fill: "forwards",
-    easing: "ease"
-  })
-  
-})
